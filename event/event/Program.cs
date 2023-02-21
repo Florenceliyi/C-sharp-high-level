eaec66bcb1782ea32608e3f16790750a7a8541c3
Florence/liyi <florenceliyii@gmail.com>
2023-02-21 21:33:09 +0800

    委托、list

---
+++ event/event/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net;
+
+namespace 事件;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Test t = new Test();
+
+        t.bibao();
+
+        //委托可以在外部调用
+        t.myFun();
+        t.myFun = Test123;
+        t.myFun();
+        //事件不可在外部调用
+        t.DoEvent();
+
+        Action a = Test123;
+
+        //无参匿名函数
+        Action b = delegate ()
+        {
+            Console.WriteLine("匿名函数逻辑");
+        };
+
+        b();
+
+        //带参匿名函数
+        Action<string, string> c = delegate (string a, string b)
+        {
+            Console.WriteLine(a+b);
+        };
+
+        c("Florence", "Xiao");
+
+        t.GetFun()();
+
+        //lambad表达式
+        //1.无参无返回
+        Action a1 = () =>
+        {
+            Console.WriteLine("无参无返回值的lambad表达式");
+        };
+        a1();
+        //2.有参
+        Action<int> a2 = (int value) =>
+        {
+            Console.WriteLine("有参数lambad表达式{0}", value);
+        };
+        a2(20);
+        //3.参数类型可以省略，参数类型和委托或事件容器一致
+        Action<int> a3 = (value) =>
+        {
+            Console.WriteLine("省略参数类型的写法{0}", value);
+        };
+        a3(520);
+        //4.有返回值
+        Func<string, int> a4 = (value) =>
+        {
+            Console.WriteLine("返回值的表达式");
+            return 3231;
+        };
+
+    }
+    static void Test123()
+    {
+        Console.Write("测试哈哈哈哈哈哈");
+    }
+    class Test
+    {
+        public Action myFun;
+
+        public event Action myEvent;
+
+
+        public Test()
+        {
+            //委托与事件的相同之处
+            myFun = TestFun;
+            myFun += TestFun;
+            myFun();
+
+            myEvent = TestEventFun;
+            myEvent += TestEventFun;
+            myEvent();
+
+            
+        }
+
+        public void DoEvent()
+        {
+            if(myEvent != null)
+            {
+                myEvent();
+            }
+        }
+        public void TestFun()
+        {
+            Console.WriteLine("Florence测试委托");
+        }
+
+        public void TestEventFun()
+        {
+            Console.WriteLine("Florence测试事件");
+        }
+
+        //作为返回值
+        public Action GetFun()
+        {
+            return delegate () {
+                Console.WriteLine("函数内部返回的一个匿名函数逻辑");
+            };
+        }
+
+        public void bibao()
+        {
+            int value = 20;
+            myFun = () =>
+            {
+                Console.WriteLine(value);
+            };
+
+            for(int i = 0; i< 10; i++)
+            {
+                int index = i;
+                myFun += () =>
+                {
+                    Console.WriteLine(index);
+                };
+            }
+        }
+
+
+    }
+    
+
+}
---
+++ 委托delegate/委托delegate/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Lesson12_委托
+{
+    delegate void MyFun();
+
+    public delegate int MyFun2(int a);
+    class Test
+    {
+        public MyFun fun;
+        public MyFun2 fun2;
+
+        public Action action;
+
+        public void TestFun(MyFun fun, MyFun2 fun2)
+        {
+            //先处理一些别的逻辑 当这些逻辑处理完了 再执行传入的函数
+            int i = 1;
+            i *= 2;
+            i += 2;
+
+            //fun();
+            //fun2(i);
+            //this.fun = fun;
+            //this.fun2 = fun2;
+        }
+
+
+        #region 增
+        public void AddFun(MyFun fun, MyFun2 fun2)
+        {
+            this.fun += fun;
+            this.fun2 += fun2;
+        }
+        #endregion
+
+        #region 删
+        public void RemoveFun(MyFun fun, MyFun2 fun2)
+        {
+            //this.fun = this.fun - fun;
+            this.fun -= fun;
+            this.fun2 -= fun2;
+        }
+        #endregion
+    }
+  
+
+    #region 知识点五 委托变量可以存储多个函数(多播委托)
+
+
+    #endregion
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("委托");
+            //专门用来装载 函数的 容器
+            MyFun f = new MyFun(Fun);
+
+            MyFun f2 = Fun3;
+
+            MyFun ff = null;
+
+            //委托存储多个函数
+            ff += f;
+            ff += f2;
+            ff();
+           
+        }
+
+        static void Fun()
+        {
+            Console.WriteLine("张三做什么");
+        }
+        static void Fun3()
+        {
+            Console.WriteLine("李四做什么");
+        }
+
+        static string Fun4()
+        {
+            return "";
+        }
+
+        static int Fun5()
+        {
+            return 1;
+        }
+
+        static void Fun6(int i, string s)
+        {
+
+        }
+
+        static int Fun2(int value)
+        {
+            return value;
+        }
+    }
+
+    //总结
+    //简单理解 委托 就是装载、传递函数的容器而已
+    //可以用委托变量 来存储函数或者传递函数的
+    //系统其实已经提供了很多委托给我们用 
+    //Action:没有返回值，参数提供了 0~16个委托给我们用
+    //Func:有返回值，参数提供了 0~16个委托给我们用
+}
